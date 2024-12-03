6f89a391dbc4c0f1399b407e3247a1c1f63f6eee
johnsci911 <johnsci911@users.noreply.github.com>
2024-12-03 11:49:26 +0000

    Dusting

--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,26 @@ Route::middleware('auth')->group(function () {
     Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/talks', [TalkController::class, 'index'])->name('talks.index');
-    Route::get('/talks/create', [TalkController::class, 'create'])->name('talks.create');
-    Route::get('/talks/{talk}', [TalkController::class, 'show'])->name('talks.show')->can('view', 'talk');
-    Route::patch('/talks/{talk}', [TalkController::class, 'update'])->name('talks.update');
-    Route::get('/talks/{talk}/edit', [TalkController::class, 'edit'])->name('talks.edit');
-    Route::post('/talks', [TalkController::class, 'store'])->name('talks.store');
+    Route::get('talks', [TalkController::class, 'index'])->name('talks.index');
+    Route::get('talks/create', [TalkController::class, 'create'])->name('talks.create');
+    Route::get('talks/{talk}', [TalkController::class, 'show'])->name('talks.show')->can('view', 'talk');
+    Route::patch('talks/{talk}', [TalkController::class, 'update'])->name('talks.update');
+    Route::get('talks/{talk}/edit', [TalkController::class, 'edit'])->name('talks.edit');
+    Route::post('talks', [TalkController::class, 'store'])->name('talks.store');
     Route::delete('talks/{talk}', [TalkController::class, 'destroy'])->name('talks.destroy');
 });
 
 require __DIR__ . '/auth.php';
 
-Route::get('/auth/redirect', function () {
+Route::get('auth/redirect', function () {
     return Socialite::driver('github')->redirect();
 });
 
-Route::get('/auth/callback', function () {
+Route::get('auth/callback', function () {
     $githubUser = Socialite::driver('github')->user();
 
     $user = User::updateOrCreate([
-        'github_id' => $githubUser->id
+        'github_id' => $githubUser->id,
     ], [
         'name' => $githubUser->name,
         'email' => $githubUser->email,
